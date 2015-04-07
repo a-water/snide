0db6cd9f248f3fcb6b7e1803f0bcfe02599ad466
Aaron Waterhouse <aaron@rPro.local>
2015-04-06 18:09:07 -0700

    Initial Commit

---
+++ Snide/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Snide
+//
+//  Created by Aaron Waterhouse on 4/6/15.
+//  Copyright (c) 2015 Aaron Waterhouse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
