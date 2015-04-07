da4ad2eae31c91fb933f647606c1e805ed7433c1
Aaron Waterhouse <aaron@rPro.local>
2015-04-06 18:34:56 -0700

    First real code commit

--- Snide/AppDelegate.m
+++ Snide/AppDelegate.m
@@ -11,13 +11,32 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (strong, nonatomic) NSStatusItem *statusItem;
+@property (assign, nonatomic) BOOL darkModeOn;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+
+    // Init the status bar button with the image
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    NSImage *menuBarImage = [NSImage imageNamed:@"bat23"];
+    // Check if system is running anything less than 10.10
+    BOOL oldBusted = (floor(NSAppKitVersionNumber) <= NSAppKitVersionNumber10_9);
+    if(!oldBusted) {
+        [menuBarImage setTemplate:YES];
+    }
+    [self.statusItem setImage:menuBarImage];
+    
+    [self.statusItem setAction:@selector(buttonClicked:)];
 }
+
+- (void)buttonClicked:(id)sender {
+    NSLog(@"bat clicked");
+}
+
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
